75cb7ccf9fe662cf3e53921cd6194fc472311ced
Shree Vatsa <vastas2002@gmail.com>
2022-07-05 10:47:49 +0530

    Initial Demo

---
+++ static/dummyEvents.js
@@ -0,0 +1,14 @@
+export const dummyEvents = [
+  {
+    price: 17,
+    from: '0xShre',
+    to: '0xSud',
+    date: '1 months ago',
+  },
+  {
+    price: 2,
+    from: '0xOwn',
+    to: '0xFel',
+    date: '4 months ago',
+  },
+]
